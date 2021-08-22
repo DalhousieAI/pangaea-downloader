2f125eba36f2d5ac45467af73edd35d019e48f6c
Scott Lowe <scott.code.lowe@gmail.com>
2021-08-22 20:06:19 +0100

    REL: Add packaging boilerplate

---
+++ pangaea_downloader/__meta__.py
@@ -0,0 +1,10 @@
+name = "pangaea-downloader"
+path = name.lower().replace("-", "_").replace(" ", "_")
+version = "0.1.dev0"
+author = "Amit Baroi"
+author_email = ""
+description = (
+    "Tools for downloading benthic habitat image datasets from https://pangaea.de/"
+)
+url = "https://github.com/DalhousieAI/pangaea-downloader"
+license = "MIT"
