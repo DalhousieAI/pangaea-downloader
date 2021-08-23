087230eb8cb1f5c87eb79a29dbe08e3427cae2fd
Amit Robert Baroi <40217034+AmitBaroi@users.noreply.github.com>
2021-08-23 16:55:50 -0300

    Added author email

--- pangaea_downloader/__meta__.py
+++ pangaea_downloader/__meta__.py
@@ -2,7 +2,7 @@ name = "pangaea-downloader"
 path = name.lower().replace("-", "_").replace(" ", "_")
 version = "0.1.dev0"
 author = "Amit Baroi"
-author_email = ""
+author_email = "amit.baroi@dal.ca"
 description = (
     "Tools for downloading benthic habitat image datasets from https://pangaea.de/"
 )
