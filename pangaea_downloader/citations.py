90bca492981413b5046a6ac2c2a9feffac243bdd
Amit Robert Baroi <amitrobertbaroi@gmail.com>
2022-01-17 15:17:34 -0400

    Added script for getting bibtex citations

---
+++ pangaea_downloader/citations.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import requests
+
+
+def get_bibtex(ds_id: str) -> str:
+    """Get the BibTex Citation of a Pangaea dataset using the dataset ID."""
+    bib_url = f"https://doi.pangaea.de/10.1594/PANGAEA.{ds_id}?format=citation_bibtex"
+    resp = requests.get(bib_url)
+    return resp.text
+
+
+def generate_citations_file(
+    pangaea_dataset: str, citations_file="../bibtex-citations.txt"
+) -> None:
+    """
+    Generate a text file with BibTex citations for all Pangaea datasets in the `pangaea_dataset` CSV file.
+
+    Parameters
+    ----------
+    pangaea_dataset : str
+        The path to the CSV file containing the full Pangaea Benthic Image Dataset.
+        It should contain a column called ``"datasets"`` with the dataset IDs.
+
+    citations_file : str
+        The path to the output text file where all the citations will be written.
+    """
+    pangaea_df = pd.read_csv(pangaea_dataset, low_memory=False)
+    ds_ids = [dataset.split("-")[-1] for dataset in pangaea_df.dataset.unique()]
+
+    # Get bibtex citations and write to file
+    with open(citations_file, "w") as f:
+        for i, ds_id in enumerate(ds_ids):
+            bibtex = get_bibtex(ds_id)
+            f.write(bibtex)
+            print(f"{(i+1)}/{len(ds_ids)} complete.")
+    print(f"All dataset BibTex citations written to file: '{citations_file}'")
+
+
+if __name__ == "__main__":
+    file = (
+        "../pangaea_2022-01-02_filtered_subsampled-1.25m-1200-1000_remove-core-surf.csv"
+    )
+    generate_citations_file(file, citations_file="../bibtex-citations.txt")
